5dd70d46115ecbd2dab386a1ffd7a53b5a7f87ac
DavidBareRosavelt <85147443+DavidBareRosavelt@users.noreply.github.com>
2021-06-01 16:26:04 +0800

    Java Files
    These are my Java Files, they are only the tutorial level ones

---
+++ javaathran/ToVariable.java
@@ -0,0 +1,16 @@
+package test.workspace.javaathran;
+
+public class ToVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer x = 9;
+		System.out.println(x.toString());
+		System.out.println(Integer.toString(23));
+		
+		Double dx = 8.5;
+		System.out.println(dx.toString());
+		System.out.println(Double.toString(4.0));
+	}
+
+}
