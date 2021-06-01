eb4ccf67f9d9f543ad10f27b6308b14ef2b4d531
DavidBareRosavelt <85147443+DavidBareRosavelt@users.noreply.github.com>
2021-06-01 16:44:18 +0800

    .java File

---
+++ WhatILearned.java
@@ -0,0 +1,23 @@
+package test.workspace.javaathran;
+
+import javax.swing.*;
+
+public class WhatILearned {
+	
+	//This is supposedly when my uncle didn't teach me for the
+	//past few days because of the pandemic
+	
+	public WhatILearned() {
+		JFrame f = new JFrame("Hello Test Window");
+		f.setVisible(true);
+		f.setSize(500, 500);
+		
+		//and other javax.swing stuff
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhatILearned wil = new WhatILearned();
+	}
+
+}
